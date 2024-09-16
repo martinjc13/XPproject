88872dcc17f5be5c8c6dfacc937186e95aa88c19
martinjc13 <martinjc13@appstate.edu>
2024-09-16 11:35:06 -0400

    added location

--- src/Food.java
+++ src/Food.java
@@ -1,13 +1,20 @@
+/**
+ * Food data structure.
+ * 
+ * @version 09/16/24
+ * @author Josh Martin
+ */
 public class Food 
 {
-    //Add location as well.
+    private String location;
     private String name;
     private double price;
 
-    public Food(String name, double price) 
+    public Food(String name, double price, String location) 
     {
         this.name = name;
         this.price = price;
+        this.location = location;
     }
 
     public String getName() 
@@ -20,8 +27,13 @@ public class Food
         return price;
     }
 
+    public String getLocation()
+    {
+        return location;
+    }
+
     public String toString() 
     {
-        return name + " - $" + price;
+        return name + " - $" + price + ", {" + location + "}";
     }
 }
